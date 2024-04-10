d8decfecce8fd0242f2cba2dcaaab09cba77c8fd
SRE <Nerocross507@gmail.com>
2024-04-10 14:01:44 +0300

    create dbmanager.py with methods

---
+++ src/dbmanager.py
@@ -0,0 +1,22 @@
+class DBManager:
+    def __init__(self):
+        pass
+
+    def get_companies_and_vacancies_count(self):
+        """Получает список всех компаний и количество вакансий у каждой компании"""
+        pass
+
+    def get_all_vacancies(self):
+        """получает список всех вакансий с указанием названия компании,
+        названия вакансии, зряплаты и ссылки на вакансию"""
+        pass
+
+    def get_avg_salary(self):
+        pass
+
+    def get_vacancies_with_higher_salary(self):
+        pass
+
+    def get_vacancies_with_keyword(self):
+        pass
+
