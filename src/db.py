15dcd6baaff78746d206e6ad3f4b05d322ce1f1e
NeroPC <Nerocross507@gmail.com>
2024-04-22 00:50:14 +0300

    fix class DBStore, work with def save_to_db

--- src/db.py
+++ src/db.py
@@ -5,25 +5,29 @@ class DBStore:
     """
     Класс для работы с базой данных
     """
-    @staticmethod
-    def create_db(db_params: dict, db_name='HH'):
-        db_name = db_name
-        conn = psycopg2.connect(db_name, *db_params)
+    def __init__(self, db_params, db_name='HH'):
+        self.db_params: dict = db_params
+        self.db_name = db_name
+
+    def create_db(self):
+        name = self.db_name
+        params = self.db_params
+        conn = psycopg2.connect(name, **params)
         conn.autocommit = True
         cur = conn.cursor()
 
-        cur.execute(f'DROP DATABASE {db_name}')
-        cur.execute(f'CREATE DATABASE {db_name}')
+        cur.execute(f'DROP DATABASE {name}')
+        cur.execute(f'CREATE DATABASE {name}')
 
         conn.close()
-        conn = psycopg2.connect(db_name, *db_params)
+        conn = psycopg2.connect(name, **params)
 
         with conn.cursor() as cur:
             cur.execute('''
-                   CREATE TABLE companies(
-                   company_id SERIAL PRIMARY KEY,
-                   company_name VARCHAR(150) NOT NULL,
-                   url_company TEXT
+                   CREATE TABLE employers(
+                   employer_id INT PRIMARY KEY,
+                   employer_name VARCHAR(150) NOT NULL,
+                   url_employer TEXT
                    )
                    ''')
 
@@ -34,19 +38,57 @@ class DBStore:
                vacancy_name VARCHAR(150) NOT NULL,
                city_name VARCHAR(100),
                publish_date DATE,
-               company_id INT ,
+               employer_id INT ,
                salary_from INTEGER,
                salary_to INTEGER,
                url_vacancy TEXT,
     
-               CONSTRAINT fk_vacancies_companies 
-               FOREIGN KEY(company_id) REFERENCES companies(company_id)
+               CONSTRAINT fk_vacancies_employers 
+               FOREIGN KEY(employer_id) REFERENCES employers(employer_id)
                )
                ''')
 
         conn.commit()
         conn.close()
 
-    def save_to_db(self):
-        with open('vacancies.json', 'r') as f:
-            self.vacancies = json.load(f)
+    def save_to_db(self, response, employer_list):
+        name = self.db_name
+        params = self.db_params
+        conn = psycopg2.connect(name, **params)
+        with conn.cursor() as cur:
+            for employer in employer_list:
+                cur.execute(
+                    '''
+                    INSERT INTO employers (employer_name)
+                    VALUES (%s)
+                    RETURNING employer_id
+                    ''',
+                    employer
+                )
+                employer_id = cur.fetchone()[0]
+            for item in response:
+                cur.execute(
+                    '''
+                    INSERT INTO vacancies (
+                    vacancy_name,
+                    city_name,
+                    publish_date,
+                    employer_id,
+                    salary_from,
+                    salary_to,
+                    url_vacancy
+                    )
+                    VALUES (%s, %s, %s, %s, %s, %s, %s)
+                    ''',
+                    (
+                        item['vacancy_name'],
+                        item['city_name'],
+                        item['publish_date'],
+                        employer_id,
+                        item['salary_from'],
+                        item['salary_to'],
+                        item['url_vacancy']
+                    )
+                )
+
+
